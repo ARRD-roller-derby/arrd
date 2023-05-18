818415362bed90c9f3ffcd888379156cc6f8597f
Kazerlelutin <b@bouteiller.contact>
2023-05-18 18:36:19 +0200

    wip

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,14 +1,19 @@
 require('dotenv').config()
-const fs = require('fs');
-const { execSync } = require('child_process');
+const fs = require('fs')
+const { execSync } = require('child_process')
 
-const token = process.env.FONTAWESOME_NPM_AUTH_TOKEN;
+const token = process.env.FONTAWESOME_NPM_AUTH_TOKEN
 if (!token) {
-  console.error('FONTAWESOME_NPM_AUTH_TOKEN environment variable is not set');
-  process.exit(1);
+  console.error('FONTAWESOME_NPM_AUTH_TOKEN environment variable is not set')
+  process.exit(1)
 }
 
-fs.writeFileSync('.npmrc', `
+fs.writeFileSync(
+  '.npmrc',
+  `
 @fortawesome:registry=https://npm.fontawesome.com/
 //npm.fontawesome.com/:_authToken=${token}
-`);
+`
+)
+
+execSync('pnpm install')
