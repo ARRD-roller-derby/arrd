c638bae9375468205eb9271cedce010a9bf7a355
Kazerlelutin <b@bouteiller.contact>
2023-05-21 00:33:50 +0200

    wip

--- apps/valhalla/entities/me/avatar.tsx
+++ apps/valhalla/entities/me/avatar.tsx
@@ -1,10 +1,8 @@
-import { useMe } from './me'
 import { ImgBubble } from 'ui'
 import { useSession } from 'next-auth/react'
 
 export const MyAvatar: React.FC = () => {
   const { data: session } = useSession()
-  const { me } = useMe()
 
   if (!session?.user)
     return <ImgBubble alt="avatar" src="static/images/profile.webp" />
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -2,6 +2,23 @@ import * as React from 'react'
 export * from '@fortawesome/react-fontawesome'
 export { faBarsFilter } from '@fortawesome/sharp-solid-svg-icons'
 
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+import timezone from 'dayjs/plugin/timezone'
+import duration from 'dayjs/plugin/duration'
+import fr from 'dayjs/locale/fr'
+
+dayjs.extend(relativeTime)
+dayjs.extend(localizedFormat)
+dayjs.extend(timezone)
+dayjs.extend(duration)
+dayjs.locale(fr)
+dayjs.tz.guess()
+dayjs.tz.setDefault('Europe/Paris')
+
+export { dayjs }
+
 export * from './buttons'
 export * from './icons'
 export * from './col/col'
